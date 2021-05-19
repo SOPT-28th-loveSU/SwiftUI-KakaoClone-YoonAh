bbdd916b77cd4157946fe377560087a4f4c38912
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-19 12:10:10 +0900

    Initial Setting

---
+++ SwiftUI-KakaoClone/SwiftUI-KakaoClone/Source/Supports/SwiftUI_KakaoCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KakaoCloneApp.swift
+//  SwiftUI-KakaoClone
+//
+//  Created by SHIN YOON AH on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KakaoCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
