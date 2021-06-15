25f5f9fbcc03d4ec144c41d0905399f040166371
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-15 23:45:31 +0900

    [ADD] ChatView 추가

---
+++ SwiftUI-KakaoClone/SwiftUI-KakaoClone/Sources/Views/Screens/ChatView.swift
@@ -0,0 +1,65 @@
+//
+//  ChatView.swift
+//  SwiftUI-KakaoClone
+//
+//  Created by SHIN YOON AH on 2021/06/15.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    let name: String
+    let image: String
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            Color(UIColor.systemGray)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center) {
+                HStack {
+                    Spacer()
+                        .frame(width: 20)
+                    
+                    Button(action: {
+                        print("dismiss")
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                    })
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image(image)
+                    .resizable()
+                    .frame(width: 97, height: 97)
+                
+                Text(name)
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .padding(.bottom, 42)
+                
+                Divider()
+                    .background(Color.white)
+                    .padding(.bottom, 20)
+                
+                MenuStack()
+                    .padding(.bottom, 30)
+            }
+        }
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView(name: "최솝트", image: "friendtabProfileImg")
+    }
+}
+
