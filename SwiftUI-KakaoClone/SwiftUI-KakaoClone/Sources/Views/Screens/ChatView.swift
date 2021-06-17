ad9244a7a75559136e1c0a092b7ac9dcb7f57568
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-17 21:28:18 +0900

    [FEAT] Chat뷰 구성

--- SwiftUI-KakaoClone/SwiftUI-KakaoClone/Sources/Views/Screens/ChatView.swift
+++ SwiftUI-KakaoClone/SwiftUI-KakaoClone/Sources/Views/Screens/ChatView.swift
@@ -10,56 +10,16 @@ import SwiftUI
 struct ChatView: View {
     let name: String
     let image: String
-    
-    @Environment(\.presentationMode) var presentationMode
+    let chat: String
     
     var body: some View {
-        ZStack {
-            Color(UIColor.systemGray)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(alignment: .center) {
-                HStack {
-                    Spacer()
-                        .frame(width: 20)
-                    
-                    Button(action: {
-                        print("dismiss")
-                        presentationMode.wrappedValue.dismiss()
-                    }, label: {
-                        Image(systemName: "xmark")
-                            .font(.system(size: 20))
-                            .foregroundColor(.white)
-                    })
-                    
-                    Spacer()
-                }
-                
-                Spacer()
-                
-                Image(image)
-                    .resizable()
-                    .frame(width: 97, height: 97)
-                
-                Text(name)
-                    .font(.system(size: 20))
-                    .foregroundColor(.white)
-                    .padding(.bottom, 42)
-                
-                Divider()
-                    .background(Color.white)
-                    .padding(.bottom, 20)
-                
-                MenuStack()
-                    .padding(.bottom, 30)
-            }
-        }
+        Text(chat)
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatView(name: "최솝트", image: "friendtabProfileImg")
+        ChatView(name: "최솝트", image: "friendtabProfileImg", chat:"안녕")
     }
 }
 
